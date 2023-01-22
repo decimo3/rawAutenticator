050333aecb7e52eb31981397d49a9319a7497ae8
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-01-22 13:10:40 -0300

    REFACTOR: Moved test instructions to trashcan file

--- Program.cs
+++ Program.cs
@@ -5,12 +5,6 @@ public static class Program
   public static void Main (string[] args)
   {
     DotNetLoadDotEnv.DotEnv.Load();
-    var conta = new Account.accountManager();
-    var usuario = new Account.User("Ruan", "palavra", Account.User.Role.Operational);
-    var token = Account.TokenManager.intokerize(usuario);
-    System.Console.WriteLine(token);
-    var json = Account.TokenManager.untokerize(token);
-    System.Console.WriteLine(json);
-    System.Console.WriteLine(Account.databaseAcess.insert("INSERT INTO teste VALUES (DEFAULT, 'Ruan');"));
+    Account.accountManager.init();
   }
 }
