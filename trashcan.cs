43c6347efe815d8dfb0224405d3fbb8fdc9b3059
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-01-27 21:22:36 -0300

    DEL: removed unnecessary files

--- trashcan.cs
+++
@@ -1,8 +0,0 @@
-/* 
-var usuario = new Account.User("Ruan", "palavra", Account.User.Role.Operational);
-var token = Account.TokenManager.intokerize(usuario);
-System.Console.WriteLine(token);
-var json = Account.TokenManager.untokerize(token);
-System.Console.WriteLine(json);
-System.Console.WriteLine(Account.databaseAcess.insert("INSERT INTO teste VALUES (DEFAULT, 'Ruan');"));
-*/
