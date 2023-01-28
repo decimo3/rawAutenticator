e5f146b70187dbb95d2aa66a269f8d0f183b2529
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-01-27 21:33:33 -0300

    NEW: sketch of intermediate class for inserting users into database

--- Schemes/userDAO.cs
+++ Schemes/userDAO.cs
@@ -1,5 +1,15 @@
 namespace Account;
 public static class userDAO
 {
-
+  public static User insertUser(Account.User user)
+  {
+    if (Account.databaseAcess.exist("users", "username", user.username)) throw new System.Exception("Usuário já existe no banco de dados!");
+    return user;
+  }
+  public static User returnUser(string user)
+  {
+    if (!Account.databaseAcess.exist("users", "username", user)) throw new System.Exception("Usuário não existe no banco de dados!");
+    var userDic = Account.databaseAcess.recover("users", "username", user);
+    return new Account.User();
+  }
 }
