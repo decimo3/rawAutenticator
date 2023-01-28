1fc00a57e15eb9b28906a9fab3cf06940fc6e05e
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-01-27 21:30:25 -0300

    NEW: externalized the validations

---
+++ Utility/formValidation.cs
@@ -0,0 +1,39 @@
+/*
+Padrão de regex para validação de password com lookahead através da leitura do artigo:
+https://www.vivaolinux.com.br/artigo/Expressoes-Regulares-Entenda-o-que-sao-Lookahead-e-Lookbehind
+*/
+
+namespace Utility;
+public static class formValidation
+{
+  public static bool username(string username)
+  {
+    var regex = new System.Text.RegularExpressions.Regex(@"[A-z][a-z]{5,15}");
+    return regex.Match(username).Success;
+  }
+  public static bool password(string password)
+  {
+    string patern = "^"; // ancora de inicio da linha
+    // lookahead que seu texto terá uma letra maiúscula
+    patern += "(?=.*[A-Z])";
+    // lookahead que seu texto terá pelo menos um símbolo
+    patern += "(?=.*[!@#$&*])";
+    // lookahead que seu texto terá pelo menos um número
+    patern += "(?=.*[0-9])";
+    // lookahead que seu texto terá uma letras minúsculas
+    patern += "(?=.*[a-z])";
+    // testa se seu texto tem entre 8 e 40 caracteres
+    patern += ".{8,40}";
+    patern += "$"; // ancora de final da linha
+    var regex = new System.Text.RegularExpressions.Regex(patern);
+    return regex.Match(password).Success;
+  }
+  public static (bool, bool) validateAnswer(string answer)
+  {
+    var regexValidate = new System.Text.RegularExpressions.Regex(@"^[yYnNSs]$");
+    var regexPositive = new System.Text.RegularExpressions.Regex(@"^[yYSs]$");
+    var isValidAnswer = regexValidate.Match(answer).Success == true;
+    var isPositiveAnswer = regexPositive.Match(answer).Success == true;
+    return (isValidAnswer, isPositiveAnswer);
+  }
+}
