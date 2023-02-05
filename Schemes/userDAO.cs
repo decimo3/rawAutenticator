3ef107fd229074d45dd3bedcbbe38e1bf8d35b04
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2023-02-05 17:55:12 -0300

    DONE: add user function working

--- Schemes/userDAO.cs
+++ Schemes/userDAO.cs
@@ -1,9 +1,12 @@
 namespace Account;
 public static class userDAO
 {
-  public static User insertUser(Account.User user)
+  // C.R.U.D. para o objeto Account.User
+  public static Account.User insertUser(Account.User user)
   {
     if (Account.databaseAcess.exist("users", "username", user.username)) throw new System.Exception("Usuário já existe no banco de dados!");
+    string[] values = {user.username, user.password, user.role.ToString()};
+    Account.databaseAcess.insert("users", values);
     return user;
   }
   public static User returnUser(string user)
